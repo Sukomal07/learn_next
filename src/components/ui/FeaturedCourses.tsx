3162405149e72bb3d11b8f360aeb66c15847b20b
Sukomal07 <hypersukomal@gmail.com>
2024-02-28 21:54:50 +0530

    add hero section

---
+++ src/components/ui/FeaturedCourses.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FeaturedCourses() {
+    return (
+        <div className='text-white'>
+            featuredCourses
+        </div>
+    )
+}
+
+export default FeaturedCourses
