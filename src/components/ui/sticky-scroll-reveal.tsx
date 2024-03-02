3e3f45f34a0386d2c08d310386335aebf90b65ad
Sukomal07 <hypersukomal@gmail.com>
2024-03-02 19:56:28 +0530

    improve code

--- src/components/ui/sticky-scroll-reveal.tsx
+++ src/components/ui/sticky-scroll-reveal.tsx
@@ -1,19 +1,25 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useMotionValueEvent, useScroll } from "framer-motion";
 import { motion } from "framer-motion";
+import { cn } from "@/utils/cn";
 
 export const StickyScroll = ({
     content,
+    contentClassName,
 }: {
     content: {
         title: string;
         description: string;
+        content?: React.ReactNode | any;
     }[];
+    contentClassName?: string;
 }) => {
-    const [activeCard, setActiveCard] = React.useState(0);
+    const [activeCard, setActiveCard] = useState(0);
     const ref = useRef<any>(null);
     const { scrollYProgress } = useScroll({
+        // uncomment line 22 and comment line 23 if you DONT want the overflow container and want to have it change on the entire page scroll
+        // target: ref,
         container: ref,
         offset: ["start start", "end start"],
     });
@@ -21,13 +27,16 @@ export const StickyScroll = ({
 
     useMotionValueEvent(scrollYProgress, "change", (latest) => {
         const cardsBreakpoints = content.map((_, index) => index / cardLength);
-        const closestBreakpointIndex = cardsBreakpoints.reduce((acc, breakpoint, index) => {
-            const distance = Math.abs(latest - breakpoint);
-            if (distance < Math.abs(latest - cardsBreakpoints[acc])) {
-                return index;
-            }
-            return acc;
-        }, 0);
+        const closestBreakpointIndex = cardsBreakpoints.reduce(
+            (acc, breakpoint, index) => {
+                const distance = Math.abs(latest - breakpoint);
+                if (distance < Math.abs(latest - cardsBreakpoints[acc])) {
+                    return index;
+                }
+                return acc;
+            },
+            0
+        );
         setActiveCard(closestBreakpointIndex);
     });
 
@@ -46,9 +55,10 @@ export const StickyScroll = ({
             animate={{
                 backgroundColor: backgroundColors[activeCard % backgroundColors.length],
             }}
-            className="h-[30rem] overflow-hidden flex justify-center relative space-x-10 rounded-md p-10"
+            className="h-[30rem] overflow-y-auto flex justify-center relative space-x-10 rounded-md p-10"
+            ref={ref}
         >
-            <div className="div relative overflow-y-auto flex items-start px-4" ref={ref}>
+            <div className="div relative flex items-start px-4">
                 <div className="max-w-2xl">
                     {content.map((item, index) => (
                         <div key={item.title + index} className="my-20">
@@ -83,8 +93,13 @@ export const StickyScroll = ({
                 animate={{
                     background: linearGradients[activeCard % linearGradients.length],
                 }}
-                className="hidden lg:block h-60 w-80 rounded-md bg-white sticky top-10 overflow-hidden"
-            ></motion.div>
+                className={cn(
+                    "hidden lg:block h-60 w-80 rounded-md bg-white sticky top-10 overflow-hidden",
+                    contentClassName
+                )}
+            >
+                {content[activeCard].content ?? null}
+            </motion.div>
         </motion.div>
     );
 };
