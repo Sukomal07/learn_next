3e7a1a6ccb179076348be7fa6901204a242ec5b7
Sukomal07 <hypersukomal@gmail.com>
2024-03-03 20:46:50 +0530

    fix a minor issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import TestiMonialCards from "@/components/TestiMonialCards";
 
 export default function Home() {
   return (
-    <main className="min-h-screen no-scrollbar bg-black/[0.96] antialiased bg-grid-white/[0.02] flex flex-col">
+    <main className="min-h-screen no-scrollbar bg-black/[0.96] antialiased flex flex-col">
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
